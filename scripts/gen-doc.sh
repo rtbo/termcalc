40b4e59005f61f77f88961368a4b528388c67de5
Rémi Thebault <remi.thebault@gmail.com>
2024-09-08 15:29:47 +0200

    gen-doc exit failure if asciidoctor not found

--- scripts/gen-doc.sh
+++ scripts/gen-doc.sh
@@ -3,6 +3,12 @@
 SCRIPTS_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 DOC_DIR=$(dirname $SCRIPTS_DIR)/doc
 
+if ! command -v asciidoctor &> /dev/null
+then
+    echo -e "asciidoctor could not be found. Please install it first."
+    exit 1
+fi
+
 echo "Generating man page in $DOC_DIR/gen/tc.1.ansi"
 
 OLD=$(stty -g)
